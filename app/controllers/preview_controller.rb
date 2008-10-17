1728558bd274f8eb0d6dedf5da9fe5b3d4c44c32
Glenn Murray <cradle@gmail.com>
2008-10-17 16:39:55 +1100

    minimize code a bit

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -2,11 +2,7 @@ class PreviewController < ApplicationController
   skip_before_filter :verify_authenticity_token
   layout false
   def show
-    page = get_page
-    params.fetch('part', []).each do |i, attrs|
-      page.parts << PagePart.new(attrs)
-    end
-    page.process(request,response)
+    construct_page.process(request,response)
     @performed_render = true
   end
   
@@ -20,13 +16,16 @@ class PreviewController < ApplicationController
     end
   end
   
-  def get_page
+  def construct_page
     if request.referer =~ %r{/admin/pages/(\d+)/child/new}
       page = page_class.new(params['page'])
       page.parent = Page.find($1)
     elsif request.referer =~ %r{/admin/pages/edit/(\d+)}
       page = Page.find($1)
       page.attributes = params['page']
+    end
+    params.fetch('part', []).each do |i, attrs|
+      page.parts << PagePart.new(attrs)
     end
     return page
   end
